5b66c1e298ec1d298275cf4aafe041d38c66ee24
TheJonaCode <mcjhona069@gmail.com>
2023-09-14 11:33:44 -0600

    modificate a movie option

--- js/home-script.js
+++ js/home-script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
           
     // ----- DOM ELEMENTS
     //Title cineME
-    const cinemeTitle = document.querySelector(".cineme-title"); //Title
+    const cinemeUser = document.querySelector(".cineme-user"); //Title
 
     // ----- SHOWING INFO
     //Título
